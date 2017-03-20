af3d4475d448faab132d730835cc35a08655e152
John Curcio <jmarrvin@gmail.com>
2017-03-20 18:56:48 -0400

    working on matching

--- numerov.py
+++ numerov.py
@@ -12,8 +12,9 @@ def f(epsilon, x, omega=1):
     return externalPotential(x, omega=omega) - epsilon
 
 def numerov(epsilon, omega=1, xMax = 1, xMin=0, N=1000):
-    h = abs(xMax - xMin)/N
+    h = (xMax - xMin)/N
     xVals = [xMin + i*h for i in range(N+1)]
+    assert(abs(xVals[-1] - xMax) < 10**-5)
     fVals = [f(epsilon, xVals[i], omega) for i in range(N+1)]
     phiVals = [None]*(N+1)
     phiVals[0] = 0
@@ -26,7 +27,7 @@ def numerov(epsilon, omega=1, xMax = 1, xMin=0, N=1000):
     return phiVals
 
 def normalizedPhiVals(phiVals, xMax, xMin, N):
-    h = abs(xMax - xMin)/N
+    h = (xMax - xMin)/N
     C = 0
     for i in range(1, N):
         C += (2*h/3)*(1 + (i%2)) * phiVals[i]**2
@@ -49,7 +50,7 @@ def getWaveFunctionVals():
     xMax = 5
     N = 10000
     #defining the following 2 because useful for plotting
-    h = abs(xMax - xMin)/N
+    h = (xMax - xMin)/N
     xVals = [xMin + i*h for i in range(N+1)]
     # 2D list. rows correspond to series from numerov recursion. columns 
     # correspond to column'th value in the recursion  over all epsilons
@@ -92,25 +93,43 @@ def shoot(epsilon1, epsilon2, omega, xMin, xMax, N, threshold=10**-3):
         # if phiLo * phiMid < 0:
         if (phiLo > 0 and phiMid < 0) or (phiLo < 0 and phiMid > 0):
             epsilonHi = epsilonMid
+            phiHi = phiMid
         else:
             epsilonLo = epsilonMid
+            phiLo = phiMid
         epsilonMid = (epsilonHi + epsilonLo)/2
         phiMid = numerov(epsilonMid, omega=omega, xMax = xMax, xMin=xMin, N=N)[-1]
         # print("%.4f %.4f" % (epsilonMid, phiMid))
     return epsilonMid
 
-def getEigenvaluesByShooting(omega=1, xMax = 15, xMin=-15, N=1000, threshold=10**-3):
-    numEigenvalues = 20 # remember to change this to 20
-    # Have guesses of eigenvalues - epsilon. 
-    # Solve for values of epsilon such that phi(xMax; epsilon) close to 0.
-    # Make no assumptions about eigenvalues except that they're all positive,
-    # so incrementally increase epsilon until the sign of phi(xMax, epsilon)
-    # changes. then use shoot with args epsilon and epsilon - epsilonStep to 
-    # solve for eigenvalue. Then continue incrementing as if nothing happened.
-    # Know eigenvalues will be positive.
+def match(epsilon1, epsilon2, omega, xMin, xMax, N, threshold=10**-3):
+    epsilonHi = max(epsilon1, epsilon2)
+    epsilonLo = min(epsilon1, epsilon2)
+    epsilonMid = (epsilonLo + epsilonHi)/2
+    if abs(epsilonHi - epsilonLo) < threshold:
+        return epsilonMid
+    deltaHi = getDelta(epsilonHi, omega=omega, xMin=xMin, xMax=xMax, N=N)
+    deltaMid = getDelta(epsilonMid, omega=omega, xMin=xMin, xMax=xMax, N=N)
+    deltaLo = getDelta(epsilonLo, omega=omega, xMin=xMin, xMax=xMax, N=N)
+    maxIters = 500
+    iters = 0
+    while(abs(epsilonHi - epsilonLo) > threshold and iters < maxIters):
+        iters += 1
+        if (deltaHi < 0 and deltaMid > 0) or (deltaHi > 0 and deltaMid < 0):
+            deltaLo = deltaMid
+            epsilonLo = epsilonMid
+        else:
+            deltaHi = deltaMid #not necessary, but w/e
+            epsilonHi = epsilonMid
+        epsilonMid = (epsilonLo + epsilonHi)/2
+        deltaMid = getDelta(epsilonMid, omega=omega, xMin=xMin, xMax=xMax, N=N)
+    return epsilonMid
+
+
+def getEigenvalues(method=shoot, omega=1, xMax = 15, xMin=-15, N=1000, threshold=10**-3, numEigenvalues=20):
     epsilon = 0 
     #sort of like the nyquist frequency
-    epsilonStep = omega/10 
+    epsilonStep = omega/10
     eigenvalues = []
     prevPhiVal = numerov(epsilon, omega=omega, xMax = xMax, xMin=xMin, N=N)[-1]
     currPhiVal = None
@@ -121,18 +140,31 @@ def getEigenvaluesByShooting(omega=1, xMax = 15, xMin=-15, N=1000, threshold=10*
         # equivalent to currPhiVal * prevPhiVal < 0, but without overflow fears
         if ((currPhiVal > 0 and prevPhiVal < 0) or
                     (currPhiVal < 0 and prevPhiVal > 0)):
-            shot = shoot(epsilon1=epsilon, epsilon2=epsilon - epsilonStep, 
+            eigenvalue = method(epsilon1=epsilon, epsilon2=epsilon - epsilonStep, 
                 omega=omega, xMax = xMax, xMin=xMin, N=N, 
                 threshold=threshold)
-            eigenvalues.append(shot)
+            eigenvalues.append(eigenvalue)
         prevPhiVal = currPhiVal
     return eigenvalues
 
-def match(epsilon1, epsilon2, omega, xMin, xMax, N, threshold=10**-3):
+
+def getDelta(epsilon=1.5, omega=1, xMin=-10.0, xMax=10.0, N=10000):
     M = N//2
-
-
-
+    h = (xMax - xMin)/N
+    phiLeftVals = numerov(epsilon, omega=omega, xMax = xMax/2, xMin=xMin, N=N//2)
+    phiRightVals = numerov(epsilon, omega=omega, xMax = xMin, xMin=xMax/2, N=N//2)
+    # scaling - don't actually need to normalize the other values, since
+    # we don't use them to compute delta
+    CLeft = (phiLeftVals[-1])
+    phiLeftVals[-1] /= CLeft
+    phiLeftVals[-2] /= CLeft
+    CRight = (phiRightVals[-1])
+    phiRightVals[-1] /= CRight
+    phiRightVals[-2] /= CRight
+    delta = phiLeftVals[-2] + phiRightVals[-2] 
+    delta -= (2 + f(epsilon, xMax/2, omega=omega)*h**2)*phiLeftVals[-1]
+    delta /= h
+    return delta
 
 def foo():
     N = 10
@@ -151,13 +183,14 @@ def foo():
 
 def plotTrueWaveFunctions(eigenvalues, omega=1.0, xMin=-10, xMax=10, N=10000):
     #defining the following 2 because useful for plotting
-    h = abs(xMax - xMin)/N
+    h = (xMax - xMin)/N
     xVals = [(xMin + i*h)*omega for i in range(N+1)]
     try:
         import matplotlib.pyplot as plt    
         for eigenvalue in eigenvalues:
             waveFunctionVals = getPhiVals(eigenvalue, omega=omega, xMax = xMax, xMin=xMin, N=N)
             plt.plot(xVals, waveFunctionVals)
+        plt.ylim(-1, 1)
         plt.show()
     except:
         pass
@@ -169,9 +202,30 @@ if __name__ == '__main__':
     # foo()
     # e = shoot(epsilon1=0.4, epsilon2=0.55, 
     #     omega=1, xMin=-11, xMax=5, N=10000, threshold=10**-5)
-    # print("e is", e)
-    eigenvalues = getEigenvaluesByShooting(threshold=10**-10)
-    print(eigenvalues)
-    plotTrueWaveFunctions(eigenvalues)    
-    # plotTrueWaveFunctions([(n + 0.5) for n in range(4)])
+    # e = match(epsilon1=0.4, epsilon2=0.55, 
+    #     omega=1, xMin=-11, xMax=5, N=10000, threshold=10**-5)
     
+    # print("e is", e)
+
+    # eigenvalues = getEigenvalues(method=shoot, threshold=10**-10)
+    # print(eigenvalues)
+    # plotTrueWaveFunctions(eigenvalues)    
+    
+    # plotTrueWaveFunctions([(n + 0.5) for n in range(20)])
+
+    # print(match(epsilon1=5.49, epsilon2=5.51, omega=1, xMin=-10, xMax=10, N=10000, threshold=10**-6))
+
+    # print(getEigenvalues(method=match, numEigenvalues=10))
+
+    deltas = []
+    epsilonVals = [0.1*e + 3 for e in range(400)]
+    for epsilon in epsilonVals:
+        deltas.append(getDelta(epsilon, xMin=-10, xMax=10))
+    try:
+        import matplotlib.pyplot as plt
+        plt.plot(epsilonVals, deltas)
+        plt.ylim(-1, 1)
+        plt.show()
+    except:
+        pass
+    # print(deltas)
